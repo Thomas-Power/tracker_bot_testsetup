a8c3fbbf0d86492ee38cf106f67fa656d6dbe06b
Thomas Power <tpaschalpower@gmail.com>
2021-07-11 21:35:38 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,50 @@
 from BotRequests import BotRequests
 from flask import Flask, request, jsonify
+import telebot
+import requests
 
-app = Flask(__name__)
+bot = telebot.TeleBot(__name__)
 
 bot_requests = BotRequests()
 
-
-@app.route('/')
-def hello_world():
-    return "Hello world"
-
-@app.route('/update_position')
-def update_position():
+@bot.route('/update_position ?(.*)')
+def update_position(message, cmd):
+    chat_dest = message['chat']['id']
     try:
-        data = request.args
-        status = bot_requests.update_position(data)
-        return status
+        username = message["user"]["username"]
+        data = message["text"].split(" ")
+        status = bot_requests.update_position(data, username)
+        bot.send_message(chat_dest, status)
     except:
-        return "Update unsuccessful"
+        bot.send_message(chat_dest, "Update unsuccessful")
 
-@app.route('/show_user')
+@bot.route('/show_user')
 def show_user():
+    chat_dest = message['chat']['id']
     try:
-        data = request.args
+        data = message["text"]
         address = bot_requests.show_user(data)
-        return address
+        bot.send_message(chat_dest, status)
     except:
-        return "Python error"
+        bot.send_message(chat_dest, "Python error")
 
-@app.route('/show_all')
+@bot.route('/show_all')
 def show_all():
     try:
         address = bot_requests.show_all()
-        return address
+        bot.send_message(chat_dest, address)
     except:
         return "Python error"
 
-@app.route('/show_ticker')
+@bot.route('/show_ticker')
 def show_ticker():
     try:
-        data = request.args
+        data = message["text"]
         address = bot_requests.show_ticker(data)
-        return address
+        bot.send_message(chat_dest, address)
     except:
         return "Python error"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.config['api_key'] = '1899234700:AAF-8-vXDtSJ6jJHPnSfsCLfCVAGe3StKEw'
+    app.poll(debug=True)
