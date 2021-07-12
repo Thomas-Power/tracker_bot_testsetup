c9be6db4606f476465f2ec3bd636f4805b4884fe
Thomas Power <tpaschalpower@gmail.com>
2021-07-12 07:48:59 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,43 +6,49 @@ bot = telebot.TeleBot(__name__)
 
 bot_requests = BotRequests()
 
-@bot.route('/update_position ?(.*)')
+@bot.message_handler(commands=['update_position'])
 def update_position(message, cmd):
     chat_dest = message['chat']['id']
     try:
         username = message["user"]["username"]
         data = message["text"].split(" ")
         status = bot_requests.update_position(data, username)
-        bot.send_message(chat_dest, status)
+        bot.reply_to(status)
+        #bot.send_message(chat_dest, status)
     except:
         bot.send_message(chat_dest, "Update unsuccessful")
 
-@bot.route('/show_user')
+@bot.message_handler(commands=['show_user'])
 def show_user():
     chat_dest = message['chat']['id']
     try:
         data = message["text"]
         address = bot_requests.show_user(data)
-        bot.send_message(chat_dest, status)
+        bot.reply_to(address)
+        #bot.send_message(chat_dest, address)
     except:
-        bot.send_message(chat_dest, "Python error")
+        bot.reply_to("Python error")
+        #bot.send_message(chat_dest, "Python error")
 
-@bot.route('/show_all')
+@bot.message_handler(commands=['show_all'])
 def show_all():
     try:
         address = bot_requests.show_all()
         bot.send_message(chat_dest, address)
     except:
-        return "Python error"
+        bot.reply_to("Python error")
+        #bot.send_message(chat_dest, "Python error")
 
-@bot.route('/show_ticker')
+@bot.route(commands=['show_ticker'])
 def show_ticker():
     try:
         data = message["text"]
         address = bot_requests.show_ticker(data)
-        bot.send_message(chat_dest, address)
+        bot.reply_to(address)
+        #bot.send_message(chat_dest, address)
     except:
-        return "Python error"
+        bot.reply_to("Python error")
+        #bot.send_message(chat_dest, "Python error")
 
 if __name__ == '__main__':
     bot.config['api_key'] = '1899234700:AAF-8-vXDtSJ6jJHPnSfsCLfCVAGe3StKEw'
