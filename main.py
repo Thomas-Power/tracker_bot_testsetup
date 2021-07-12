241e9edf20f106647350d2e607d426ddd39aa377
Thomas Power <tpaschalpower@gmail.com>
2021-07-12 09:54:19 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -7,46 +7,60 @@ bot_requests = BotRequests()
 
 @bot.message_handler(commands=['update_position'])
 def update_position(message):
-    chat_dest = message.json["chat"]["id"]
     try:
-        username = message.json["chat"]["first_name"]
-        data = message.json["text"].split(" ")[1:]
-        status = bot_requests.update_position(data, username)
-        bot.send_message(chat_dest, status)
+        chat_dest = message.json["chat"]["id"]
+        try:
+            username = message.json["chat"]["first_name"]
+            data = message.json["text"].split(" ")[1:]
+            status = bot_requests.update_position(data, username)
+            bot.send_message(chat_dest, status)
+        except:
+            bot.send_message(chat_dest, "Update unsuccessful")
     except:
-        bot.send_message(chat_dest, "Update unsuccessful")
+        pass
 
 @bot.message_handler(commands=['show_user'])
 def show_user(message):
-    chat_dest = message['chat']['id']
     try:
-    data =  message.json["text"].split(" ")[1].upper()
-    address = bot_requests.show_user(data)
-    bot.reply_to(message, address)
-    bot.send_message(chat_dest, address)
+        chat_dest = message.json["chat"]["id"]
+        try:
+            data =  message.json["text"].split(" ")[1].upper()
+            address = bot_requests.show_user(data)
+            bot.reply_to(message, address)
+            bot.send_message(chat_dest, address)
+        except:
+            bot.send_message(chat_dest, "Python error")
     except:
-        bot.send_message(chat_dest, "Python error")
+        pass
 
 @bot.message_handler(commands=['show_all'])
 def show_all(message):
-    #try:
-    address = bot_requests.show_all()
-    bot.reply_to(message, address)
-    #bot.send_message(chat_dest, address)
-    #except:
-    #    bot.reply_to("Python error")
-        #bot.send_message(chat_dest, "Python error")
+    try:
+        chat_dest = message.json['chat']['id']
+        try:
+            address = bot_requests.show_all()
+            #bot.reply_to(message, address)
+            bot.send_message(chat_dest, address)
+        except:
+            #bot.reply_to("Python error")
+            bot.send_message(chat_dest, "Python error")
+    except:
+        pass
 
 @bot.message_handler(commands=['show_ticker'])
 def show_ticker(message):
-    #try:
-    data =  message.json["text"].split(" ")[1].upper()
-    address = bot_requests.show_ticker(data)
-    bot.reply_to(message, address)
-        #bot.send_message(chat_dest, address)
-    #except:
-    #    bot.reply_to("Python error")
-        #bot.send_message(chat_dest, "Python error")
+    try:
+        try:
+            chat_dest = message.json["chat"]["id"]
+            data =  message.json["text"].split(" ")[1].upper()
+            address = bot_requests.show_ticker(data)
+            #bot.reply_to(message, address)
+            bot.send_message(chat_dest, address)
+        except:
+            #bot.reply_to("Python error")
+            bot.send_message(chat_dest, "Python error")
+    except:
+        pass
 
 if __name__ == '__main__':
     print("working")
