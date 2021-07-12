1de8e65588c988d466a10a46edc49ee323c5c4d5
Thomas Power <tpaschalpower@gmail.com>
2021-07-12 10:15:27 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from BotRequests import BotRequests
 import telebot
 
-bot = telebot.TeleBot('1899234700:AAF-8-vXDtSJ6jJHPnSfsCLfCVAGe3StKEw')
+app = telebot.TeleBot('1899234700:AAF-8-vXDtSJ6jJHPnSfsCLfCVAGe3StKEw')
 
 bot_requests = BotRequests()
 
-@bot.message_handler(commands=['update_position'])
+@app.message_handler(commands=['update_position'])
 def update_position(message):
     try:
         chat_dest = message.json["chat"]["id"]
@@ -13,55 +13,55 @@ def update_position(message):
             username = message.json["chat"]["first_name"]
             data = message.json["text"].split(" ")[1:]
             status = bot_requests.update_position(data, username)
-            bot.send_message(chat_dest, status)
+            app.send_message(chat_dest, status)
         except:
-            bot.send_message(chat_dest, "Update unsuccessful")
+            app.send_message(chat_dest, "Update unsuccessful")
     except:
         pass
 
-@bot.message_handler(commands=['show_user'])
+@app.message_handler(commands=['show_user'])
 def show_user(message):
     try:
         chat_dest = message.json["chat"]["id"]
         try:
             data =  message.json["text"].split(" ")[1].upper()
             address = bot_requests.show_user(data)
-            bot.reply_to(message, address)
-            bot.send_message(chat_dest, address)
+            app.reply_to(message, address)
+            app.send_message(chat_dest, address)
         except:
-            bot.send_message(chat_dest, "Python error")
+            app.send_message(chat_dest, "Python error")
     except:
         pass
 
-@bot.message_handler(commands=['show_all'])
+@app.message_handler(commands=['show_all'])
 def show_all(message):
     try:
         chat_dest = message.json['chat']['id']
         try:
             address = bot_requests.show_all()
-            #bot.reply_to(message, address)
-            bot.send_message(chat_dest, address)
+            #app.reply_to(message, address)
+            app.send_message(chat_dest, address)
         except:
-            #bot.reply_to("Python error")
-            bot.send_message(chat_dest, "Python error")
+            #app.reply_to("Python error")
+            app.send_message(chat_dest, "Python error")
     except:
         pass
 
-@bot.message_handler(commands=['show_ticker'])
+@app.message_handler(commands=['show_ticker'])
 def show_ticker(message):
     try:
         try:
             chat_dest = message.json["chat"]["id"]
             data =  message.json["text"].split(" ")[1].upper()
             address = bot_requests.show_ticker(data)
-            #bot.reply_to(message, address)
-            bot.send_message(chat_dest, address)
+            #app.reply_to(message, address)
+            app.send_message(chat_dest, address)
         except:
-            #bot.reply_to("Python error")
-            bot.send_message(chat_dest, "Python error")
+            #app.reply_to("Python error")
+            app.send_message(chat_dest, "Python error")
     except:
         pass
 
 if __name__ == '__main__':
     print("working")
-    bot.polling()
+    app.polling()
